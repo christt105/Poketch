72745d838babc41780fb864909dbbd1d7f7771f4
VictorSegura99 <36154682+VictorSegura99@users.noreply.github.com>
2021-10-01 09:34:56 +0200

    Code Review del profe arreglado

--- Poketch/Assets/Scripts/Functions/AnalogClock.cs
+++ Poketch/Assets/Scripts/Functions/AnalogClock.cs
@@ -8,7 +8,7 @@ public class AnalogClock : Function
     [SerializeField] RectTransform shortHand;
     [SerializeField] RectTransform longHand;
 
-    Vector2 time = Vector2.zero;
+    Vector2Int time = Vector2Int.zero;
 
     private void Update()
     {
@@ -20,15 +20,20 @@ public class AnalogClock : Function
         int hour = DateTime.Now.Hour;
         int minutes = DateTime.Now.Minute;
 
-        if (time == new Vector2(hour, minutes)) return;
+        if (time.y == minutes && time.x == hour) return;
 
+        // Addapt 24h format to 12h format
         int angleH = hour >= 12 ? hour - 12 : hour;
+
+        // 30 comes from 360 divided by 12. Degrees for each hour.
+        // Minutes * 0.5f is for adjusting the hour hand to the current minute.
         float angle = angleH * 30 + minutes * 0.5f;
         shortHand.rotation = Quaternion.Euler(new Vector3(0, 0, -angle));
 
+        // 6 comes from dividing 360 by 60. Degrees for each minute.
         angle = minutes * 6;
         longHand.rotation = Quaternion.Euler(new Vector3(0, 0, -angle));
 
-        time = new Vector2(hour, minutes);
+        time.Set(hour, minutes);
     }
 }
