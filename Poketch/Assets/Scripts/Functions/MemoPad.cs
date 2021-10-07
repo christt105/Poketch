83b9a8d66ce8b9d1c6d7a187b928410930133328
PolHostRex <polkira7@gmail.com>
2021-10-07 20:04:52 +0200

    changing buttons

--- Poketch/Assets/Scripts/Functions/MemoPad.cs
+++ Poketch/Assets/Scripts/Functions/MemoPad.cs
@@ -13,6 +13,17 @@ public class MemoPad : Function
     private Button m_ButtonPaint;
     [SerializeField]
     private RawImage m_ImageToPaint;
+    [SerializeField]
+    private Sprite m_PaintSpritePressed;
+    [SerializeField]
+    private Sprite m_PaintSpriteIdle;
+    [SerializeField]
+    private Sprite m_EraseSpritePressed;
+    [SerializeField]
+    private Sprite m_EraseSpriteIdle;
+
+    private Image PaintButton;
+    private Image EraseButton;
 
     public static readonly int width = 156 / 2;
     public static readonly int height = 150 / 2;
@@ -42,7 +53,12 @@ public class MemoPad : Function
     {
         m_ButtonErase.onClick.AddListener(() => ChangeState(ACTION_STATE.ERASING));
         m_ButtonPaint.onClick.AddListener(() => ChangeState(ACTION_STATE.PAINTING));
-        m_ButtonPaint.Select();
+
+        // Put the pressed sprite to the paint button at the beggining.
+        PaintButton = m_ButtonPaint.gameObject.GetComponent<Image>();
+        EraseButton = m_ButtonErase.gameObject.GetComponent<Image>();
+        PaintButton.sprite = m_PaintSpritePressed;
+
 
         // Connect the ScreenTouched() method to the PaintTexture signal.
         PaintTexture.onScreenTouched += ScreenTouched;
@@ -59,18 +75,39 @@ public class MemoPad : Function
 
     public override void OnChange()
     {
+        ChangeState(ACTION_STATE.PAINTING, true);
         onResetTexture();
     }
 
     // Method that change the current action state and perform the logic and graphic part.
     // This method is called when one of the two buttons is clicked.
     // @action is an ACTION_STATE variable used to know which action we want to perform.
-    private void ChangeState(ACTION_STATE action)
+    private void ChangeState(ACTION_STATE action, bool onChange = false)
     {
+        if (action == ACTION_STATE.PAINTING)
+        {
+            if (current_state == ACTION_STATE.ERASING)
+            {
+                EraseButton.sprite = m_EraseSpriteIdle;
+                PaintButton.sprite = m_PaintSpritePressed;
+            }
+        }
+        else
+        {
+            if (current_state == ACTION_STATE.PAINTING)
+            {
+                EraseButton.sprite = m_EraseSpritePressed;
+                PaintButton.sprite = m_PaintSpriteIdle;
+            }
+        }
+
         if (action != current_state)
         {
             current_state = action;
-            SoundManager.Instance.PlaySFX(SoundManager.SFX.Button);
+            if (!onChange)
+            {
+                SoundManager.Instance.PlaySFX(SoundManager.SFX.Button);
+            }
         }
         else
         {
