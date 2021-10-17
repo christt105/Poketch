45ebfb4207a5566de695e0a91cf12df7adf2a9a6
PolHostRex <polkira7@gmail.com>
2021-10-17 18:11:46 +0200

    zahori mamadisimo

--- Poketch/Assets/Scripts/Functions/Zahori.cs
+++ Poketch/Assets/Scripts/Functions/Zahori.cs
@@ -1,18 +1,44 @@
-using System.Collections;
-using System.Collections.Generic;
+using SimpleJSON;
 using UnityEngine;
 
 public class Zahori : Function
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField]
+    private GameObject circle;
+
+    private Canvas canvas;
+
+    public override void OnCreate(JSONNode jsonObject)
     {
-        
+        canvas = FindObjectOfType<Canvas>();
+    }
+    public override void OnChange()
+    {
+        circle.SetActive(false);
     }
 
-    // Update is called once per frame
-    void Update()
+    private void OnEnable()
     {
-        
+        Signals.onScreenTouched += ScreenTouched;
+    }
+
+    private void OnDisable()
+    {
+        Signals.onScreenTouched -= ScreenTouched;
+    }
+
+    private void ScreenTouched()
+    {
+        if (!circle.activeInHierarchy)
+            ActivateCircle();
+    }
+
+    private void ActivateCircle()
+    {
+        circle.SetActive(true);
+
+        Vector2 pos;
+        RectTransformUtility.ScreenPointToLocalPointInRectangle(canvas.transform as RectTransform, Input.mousePosition, canvas.worldCamera, out pos);
+        circle.transform.position = canvas.transform.TransformPoint(pos);
     }
 }
