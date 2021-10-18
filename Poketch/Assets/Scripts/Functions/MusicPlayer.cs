589a943eea6c21c6adafc1e650ccaea4572333e7
VictorSegura99 <36154682+VictorSegura99@users.noreply.github.com>
2021-10-18 12:06:00 +0200

    Music Player 1.0

---
+++ Poketch/Assets/Scripts/Functions/MusicPlayer.cs
@@ -0,0 +1,123 @@
+using SimpleJSON;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MusicPlayer : Function
+{
+    [Header("Music Player")]
+    [SerializeField] AudioClip[] songs;
+    [SerializeField] Text songName;
+    [SerializeField] Button playButton;
+    [SerializeField] Slider songProgression;
+    [SerializeField] Text currentTimeText;
+    [SerializeField] Text songText;
+
+    [SerializeField] Sprite play;
+    [SerializeField] Sprite pause;
+
+    AudioSource player;
+    int currentSong = 0;
+    float currentSongTime = 0;
+    float completeTime = 0;
+
+    public override void OnCreate(JSONNode jsonObject)
+    {
+        player = GetComponent<AudioSource>();
+
+        player.clip = songs[currentSong];
+        completeTime = player.clip.length;
+        songName.text = player.clip.name;
+        ApplyTimeToUI(songText, completeTime);
+    }
+
+    private void Update()
+    {
+        if (!player.isPlaying) return;
+
+        currentSongTime += Time.deltaTime;
+        ApplyTimeToUI(currentTimeText, currentSongTime);
+
+        songProgression.value = currentSongTime / completeTime;
+
+        if (currentSongTime >= completeTime)
+        {
+            NextTrack();
+        }
+    }
+
+    public void PlayPressed()
+    {
+        playButton.image.sprite = playButton.image.sprite == play ? pause : play;
+
+        if (player.isPlaying)
+        {
+            player.Stop();
+        }
+        else
+        {
+            player.Play();
+        }
+    }
+
+    public void NextTrack()
+    {
+        ChangeTrack(1, true);
+    }
+
+    public void PreviousTrack()
+    {
+        if (currentSongTime >= 2)
+        {
+            currentSongTime = 0;
+            ApplyTimeToUI(currentTimeText, currentSongTime);
+            player.time = 0;
+        }
+        else
+        {
+            ChangeTrack(-1, player.isPlaying);
+        }
+    }
+
+    void ChangeTrack(int change, bool play)
+    {
+        currentSongTime = 0;
+        ApplyTimeToUI(currentTimeText, currentSongTime);
+
+        currentSong += change;
+
+        if (currentSong < 0) currentSong = songs.Length - 1;
+        if (currentSong >= songs.Length) currentSong = 0;
+
+        player.clip = songs[currentSong];
+        completeTime = player.clip.length;
+        ApplyTimeToUI(songText, completeTime);
+        songName.text = player.clip.name;
+
+        if (play && !player.isPlaying)
+        {
+            playButton.image.sprite = pause;
+            player.Play();
+        }
+    }
+
+    void ApplyTimeToUI(Text text, float seconds)
+    {
+        int minutes = Mathf.FloorToInt(seconds / 60);
+        int secs = Mathf.FloorToInt(seconds - minutes * 60);
+        string minutesS = minutes > 9 ? minutes.ToString() : "0" + minutes;
+        string secsS = secs > 9 ? secs.ToString() : "0" + secs;
+
+        text.text = minutesS + ":" + secsS;
+    }
+
+    public void SliderChange()
+    {
+        if (Mathf.Approximately(songProgression.value, currentSongTime / completeTime)) return;
+
+        currentSongTime = completeTime * songProgression.value;
+        player.time = songProgression.value;
+        ApplyTimeToUI(currentTimeText, currentSongTime);
+    }
+}
